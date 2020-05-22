ec4f7fda9fd0f2fc999c45339870a04286506749
hyoni0817 <dudu1104@naver.com>
2020-05-23 04:05:36 +0900

    수정 버튼에 onClick 속성 추가

--- component/TodoList.jsx
+++ component/TodoList.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const TodoList = (props) => {
     const {todoInfo} = props;
 
+    const onClickUpdate = () => {
+
+    }
+    
     return (
         <>
             <li>
@@ -12,7 +16,7 @@ const TodoList = (props) => {
                     <p>내용: {todoInfo.contents}</p>
                     <p>작성일: {todoInfo.writeDate}</p>
                     {todoInfo.finalWriteDate === 0 ? null:<p>최종수정일: {todoInfo.finalWriteDate}</p>}
-                    <button>수정</button><button>삭제</button>
+                    <button onClick={onClickUpdate}>수정</button><button>삭제</button>
                 </div>
             </li>
 
